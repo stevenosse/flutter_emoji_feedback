91136b83cf9558fef1576ccf1db63cb688c4ff42
Steve NOSSE <nossesteve@gmail.com>
2024-04-03 18:14:22 +0200

    New presets, new customization options

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -5,7 +5,7 @@
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
-// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
 
 import 'package:flutter/widgets.dart';
 
@@ -17,6 +17,21 @@ class Assets {
   static const String classicGood = 'assets/classic_good.svg';
   static const String classicTerrible = 'assets/classic_terrible.svg';
   static const String classicVeryGood = 'assets/classic_very_good.svg';
+  static const String flatAwesome = 'assets/flat_awesome.svg';
+  static const String flatBad = 'assets/flat_bad.svg';
+  static const String flatGood = 'assets/flat_good.svg';
+  static const String flatTerrible = 'assets/flat_terrible.svg';
+  static const String flatVeryGood = 'assets/flat_very_good.svg';
+  static const String hdAwesome = 'assets/hd_awesome.svg';
+  static const String hdBad = 'assets/hd_bad.svg';
+  static const String hdGood = 'assets/hd_good.svg';
+  static const String hdTerrible = 'assets/hd_terrible.svg';
+  static const String hdVeryGood = 'assets/hd_very_good.svg';
+  static const String threeDAwesome = 'assets/three_d_awesome.svg';
+  static const String threeDBad = 'assets/three_d_bad.svg';
+  static const String threeDGood = 'assets/three_d_good.svg';
+  static const String threeDTerrible = 'assets/three_d_terrible.svg';
+  static const String threeDVeryGood = 'assets/three_d_very_good.svg';
 
   /// List of all assets
   List<String> get values => [
@@ -24,7 +39,22 @@ class Assets {
         classicBad,
         classicGood,
         classicTerrible,
-        classicVeryGood
+        classicVeryGood,
+        flatAwesome,
+        flatBad,
+        flatGood,
+        flatTerrible,
+        flatVeryGood,
+        hdAwesome,
+        hdBad,
+        hdGood,
+        hdTerrible,
+        hdVeryGood,
+        threeDAwesome,
+        threeDBad,
+        threeDGood,
+        threeDTerrible,
+        threeDVeryGood
       ];
 }
 
@@ -86,7 +116,16 @@ class AssetGenImage {
     );
   }
 
-  ImageProvider provider() => AssetImage(_assetName);
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
 
   String get path => _assetName;
 
